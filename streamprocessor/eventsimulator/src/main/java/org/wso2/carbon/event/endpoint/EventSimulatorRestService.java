64cfb3c37eca7664ab532ca6c37afb6534ab8cbb
mathuriga <tmathu24@yahoo.com>
2016-12-11 14:13:52 +0530

    Added java doc for single event simulator related files

--- streamprocessor/eventsimulator/src/main/java/org/wso2/carbon/event/endpoint/EventSimulatorRestService.java
+++ streamprocessor/eventsimulator/src/main/java/org/wso2/carbon/event/endpoint/EventSimulatorRestService.java
@@ -3,23 +3,17 @@ package org.wso2.carbon.event.endpoint;
 
 import com.google.gson.Gson;
 import org.apache.axis2.deployment.DeploymentException;
-import org.wso2.carbon.event.querydeployer.bean.Event;
 import org.wso2.carbon.event.simulator.bean.FeedSimulationConfig;
 import org.wso2.carbon.event.simulator.csvFeedSimulation.CSVFileConfig;
 import org.wso2.carbon.event.simulator.csvFeedSimulation.core.CSVFeedEventSimulator;
 import org.wso2.carbon.event.simulator.csvFeedSimulation.core.FileDeployer;
-import org.wso2.carbon.event.simulator.csvFeedSimulation.core.FileDto;
-import org.wso2.carbon.event.simulator.randomdatafeedsimulation.bean.RandomDataSimulationConfig;
-import org.wso2.carbon.event.simulator.randomdatafeedsimulation.core.RandomDataEventSimulator;
 import org.wso2.carbon.event.simulator.singleventsimulator.SingleEventSimulationConfig;
 import org.wso2.carbon.event.simulator.singleventsimulator.SingleEventSimulator;
 import org.wso2.carbon.event.simulator.utils.EventSimulatorParser;
 import org.wso2.carbon.event.simulator.utils.EventSimulatorServiceExecutor;
 import org.wso2.msf4j.formparam.FileInfo;
 import org.wso2.msf4j.formparam.FormDataParam;
-
 import javax.ws.rs.Consumes;
-import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
@@ -32,12 +26,12 @@ import java.io.InputStream;
  * Created by mathuriga on 12/11/16.
  */
 
-@Path("/RandomEvent")
+@Path("/EventSimulation")
 public class EventSimulatorEndpoint {
 
-    //Random event generator
+
     @POST
-    @Path("/singleEvent")
+    @Path("/singleEventSimulation")
     public Response singleEventSimulation(String singleEventSimulationConfig) throws IOException {
         //JSON from String to Object
         String message = null;
@@ -53,47 +47,6 @@ public class EventSimulatorEndpoint {
         return Response.ok(jsonString, MediaType.APPLICATION_JSON)
                 .header("Access-Control-Allow-Origin", "*").build();
     }
-
-    //Random event generator
-//    @POST
-//    @Path("/demo")
-//    public Response randomDataSimulation(String eventSimulationConfig) throws IOException {
-//        String message = null;
-//
-//      //  RandomDataEventSimulator randomDataEventSimulator = RandomDataEventSimulator.getRandomDataEventSimulator();
-//        RandomDataSimulationConfig randomDataSimulationConfig=randomDataEventSimulator.configureSimulation(eventSimulationConfig);
-//
-//        if(randomDataEventSimulator.send(randomDataSimulationConfig)){
-//            message= "Event is send successfully";
-//        }
-//
-//        String jsonString = new Gson().toJson(message);
-//        return Response.ok(jsonString, MediaType.APPLICATION_JSON)
-//                    .header("Access-Control-Allow-Origin", "*").build();
-
-    //}
-
-//    @POST
-//    @Path("/csvfile")
-//    @Consumes(MediaType.MULTIPART_FORM_DATA)
-//    public Response fileFeedSimulation(@FormDataParam("file") FileInfo fileInfo,
-//                                    @FormDataParam("file") InputStream fileInputStream,
-//                                    @FormParam("inputStreamName") String inputStreamName,
-//                                    @FormParam("delimiter") char delimiter,
-//                                    @FormParam("delay") int delay){
-//
-//        String message = null;
-//        CSVFeedEventSimulator csvFeedEventSimulator=CSVFeedEventSimulator.getCSVFeedEventSimulator();
-//        FileDto fileDto=new FileDto(fileInfo,fileInputStream);
-//        CSVFileConfig csvFileConfig=new CSVFileConfig(fileInfo.getFileName(),inputStreamName,fileDto,delimiter,delay);
-//
-//        if(csvFeedEventSimulator.send(csvFileConfig)){
-//            message= "Event is send successfully";
-//        }
-//        String jsonString = new Gson().toJson(message);
-//        return Response.ok(jsonString, MediaType.APPLICATION_JSON)
-//                .header("Access-Control-Allow-Origin", "*").build();
-//    }
 
     @POST
     @Path("/fileFeedSimulation")
@@ -154,7 +107,7 @@ public class EventSimulatorEndpoint {
 
     @POST
     @Path("/feedSimulation")
-    public Response feedSimulation(String feedSimulationConfigDetails) throws DeploymentException {
+    public Response feedSimulation(String feedSimulationConfigDetails) throws DeploymentException, InterruptedException {
 
         String message = null;
 
@@ -163,6 +116,12 @@ public class EventSimulatorEndpoint {
         String jsonString = new Gson().toJson(message);
         return Response.ok(jsonString, MediaType.APPLICATION_JSON)
                 .header("Access-Control-Allow-Origin", "*").build();
+    }
+
+    @POST
+    @Path("/feedSimulation")
+    public void stopEvent(){
+
     }
 
 
